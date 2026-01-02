9f9822391ceed6ecf9702183eb8696074ff58dd3
Adam <golota.adam@gmail.com>
2026-01-02 21:25:19 +0200

    fix: proficiencies backfill script

--- scripts/backfill-save-proficiencies.ts
+++ scripts/backfill-save-proficiencies.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
+import { prisma } from "@/lib/prisma";
 import { Ability } from "@prisma/client";
 
 // tsx scripts do not automatically load Next.js env files.
